febc0bbfae8a1a7ebb2aec9e040239e46027bb54
agaolejniq <a.olejnikk@gmail.com>
2026-03-07 09:18:55 +0100

    leftmost position logic for solving single row - now without parameters - hardcoded row and hint

--- solver_logic/logic.py
+++ solver_logic/logic.py
@@ -8,19 +8,45 @@ class Puzzle:
 
     def get_grid(self):
         grid = []
-        for _ in range(len(self.col_clues)):
+        for _ in range(len(self.row_clues)):
             row = []
-            for _ in range(len(self.row_clues)):
+            for _ in range(len(self.col_clues)):
                 row.append(None)
             grid.append(row)     
         return grid
+    
+    def leftmost_position(self):
+        clues = [3, 2]
+        row = []
+        space_needed = 0
+        index = 0
+        for _ in range(10):
+            row.append(None)
+
+        for i, c in enumerate(clues): 
+            space_needed += c
+
+            while index <= space_needed:
+                if index == space_needed:
+                    if i != len(clues) - 1: #skipping last 0 - in case clue fills whole row 
+                        print(f"inserting 0 at index {index}")
+                        row[index] = 0
+                else: 
+                    row[index] = 1
+                index += 1
+            space_needed += 1
+        
+        for i, c in enumerate(row): #filling tailing cells with 0
+            if c == None:
+                row[i] = 0    
+
+        return row
 
     def solve(self):
         return "solution"
     
 rows = [[1,2,1],[1],[2,4]]
-cols = [[3,1],[4],[4,2]]
-
+cols = [[3,1],[4],[4,2],[7]]
 
 test = Puzzle(rows,cols)
-print(test.get_grid())
+print(test.leftmost_position())

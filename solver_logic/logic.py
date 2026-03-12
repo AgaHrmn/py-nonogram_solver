10fbb868fb60a9e4d78e6d1d42afcd241ccf7561
agaolejniq <a.olejnikk@gmail.com>
2026-03-13 00:13:36 +0100

    extracting columns from the grid

--- solver_logic/logic.py
+++ solver_logic/logic.py
@@ -14,19 +14,21 @@ class NonoPuzzle:
             row = []
             for _ in range(len(self.col_clues)):
                 row.append(None)
-            grid.append(row)     
+            grid.append(row)         
         return grid
     
-    def leftmost_position(self, clue, length):
+    def leftmost_position(self, clue, row):
         # calculate needed space for the clue - [3,2] - (3*1 + 0) + (2*1) 
-        # fill rest of the row with 0
+        # fill rest of the row with None
+
+        # refactor to find the leftmost valid arrangement that is consistent with the existing row state !!! Skip cells already set to 0, Respect cells already set to 1
         
+        # row = []
         space_needed = 0
         index = 0
-        row = []
 
-        for _ in range(length):
-            row.append(None)
+        # for _ in range(length):
+        #     row.append(None)
 
         for i, c in enumerate(clue): 
             space_needed += c
@@ -47,16 +49,16 @@ class NonoPuzzle:
 
         return row
 
-    def rightmost_position(self, clue, length):
+    def rightmost_position(self, clue, row):
         # reverse clue [3,2] -> [2,3] 
         # leftmost of reversed clue 1110110000
         # reverse result 0000110111
         reversed_clue = clue[::-1] # not altering the clue 
-        row = self.leftmost_position(reversed_clue, length)
-        reversed_row = row[::-1]
+        temp_row = self.leftmost_position(reversed_clue, row)
+        reversed_row = temp_row[::-1]
  
         return reversed_row
-    
+
     def find_overlap(self, leftmost, rightmost):
         overlap = []
 
@@ -73,6 +75,25 @@ class NonoPuzzle:
                     overlap[i] = None # unknown
         return overlap
     
+    def get_columns(self):
+        # grid = [
+        #     [1,2,3,4],
+        #     [5,6,7,8],
+        #     [9,10,11,12]
+        # ]
+
+        columns = []
+        i=0
+        
+        while i < len(self.grid[0]): # len(grid[0]) - row length
+            temp = []
+            for row in self.grid:
+                temp.append(row[i])
+            columns.append(temp)
+            i+=1
+
+        return columns
+
     def solve(self):
         return "solution"
     
@@ -85,8 +106,10 @@ rows = [[3,2],[1],[2,4]]
 cols = [[3,1],[4],[4,2],[7]]
 
 test = NonoPuzzle(rows,cols)
-left = test.leftmost_position(rows[0], 10)
-right = test.rightmost_position(rows[0], 10)
+# left = test.leftmost_position(rows[0], 10)
+# right = test.rightmost_position(rows[0], 10)
 
-print(f"left: {left}\nright: {right}")
-print(f"overlap: {test.find_overlap(left, right)}")
+# print(f"left: {left}\nright: {right}")
+# print(f"overlap: {test.find_overlap(left, right)}")
+print(f"grid: {test.grid}")
+print(f"get columns result: {test.get_columns()}")
